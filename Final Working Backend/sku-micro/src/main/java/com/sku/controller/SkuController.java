ff160374e4cc959379e00601b1d4360327472d64
subbumuthiah <60836367+subbumuthiah@users.noreply.github.com>
2024-05-08 18:09:45 -0500

    Added cors api support
    Can hit api from any url now

--- Final Working Backend/sku-micro/src/main/java/com/sku/controller/SkuController.java
+++ Final Working Backend/sku-micro/src/main/java/com/sku/controller/SkuController.java
@@ -1,12 +1,7 @@
 package com.sku.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.sku.request.CreateSkuRequest;
 import com.sku.response.SkuResponse;
@@ -16,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/sku")
+@CrossOrigin(origins = "*")
 public class SkuController {
 
 	@Autowired
